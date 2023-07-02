755561457524499edecb0bdecbbd9a799b0c406b
Mohamed Naser <mahmmedn19@gmail.com>
2023-07-02 08:13:45 +0300

    arrange to pkg and add necessary screen

---
+++ app/src/main/java/com/example/cinematickets/screens/TicketsScreen.kt
@@ -0,0 +1,10 @@
+/*
+ * Created by Mohamed Naser on 7/2/23, 7:12 AM, 2023.
+ *
+ * Copyright (c) 2023 All rights reserved.
+ *
+ * Last modified: 7/2/23, 7:12 AM
+ *
+ */
+
+package com.example.cinematickets.screens
