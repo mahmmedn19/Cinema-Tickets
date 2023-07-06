75070ccff0ac49fb1dfc6fabaafcbc6095514519
Mohamed Naser <mahmmedn19@gmail.com>
2023-07-06 17:50:43 +0300

    fix image rounded

--- app/src/main/java/com/example/cinematickets/composable/Image.kt
+++ app/src/main/java/com/example/cinematickets/composable/Image.kt
@@ -10,6 +10,8 @@
 package com.example.cinematickets.composable
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -23,8 +25,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
+import com.example.cinematickets.screens.generateRandomImageUrls
 
 @Composable
 fun ResizableImage(
@@ -42,6 +46,7 @@ fun ResizableImage(
         contentScale = ContentScale.Crop
     )
 }
+
 @Composable
 fun MyListImage(dataList: List<String>) {
     LazyRow(modifier = Modifier.fillMaxWidth()) {
@@ -55,4 +60,26 @@ fun MyListImage(dataList: List<String>) {
             )
         }
     }
+}
+
+@Composable
+fun MyListRounded(dataList: List<String>) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(16.dp)
+    ) {
+        items(dataList) { imageUrl ->
+            ResizableImage(
+                painter = rememberAsyncImagePainter(imageUrl),
+                modifier = Modifier
+                    .clip(RoundedCornerShape(100.dp))
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun Preview() {
+    MyListRounded(generateRandomImageUrls(10))
 }
