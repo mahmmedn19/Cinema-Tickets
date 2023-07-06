38baf381a614c3a80249a35642b5e4054988d42e
Mohamed Naser <mahmmedn19@gmail.com>
2023-07-06 17:51:14 +0300

    fix composable

--- app/src/main/java/com/example/cinematickets/composable/Chips.kt
+++ app/src/main/java/com/example/cinematickets/composable/Chips.kt
@@ -15,6 +15,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,33 +26,34 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.example.cinematickets.ui.theme.Orange
 
 @Composable
 fun TextChip(
     isSelected: Boolean,
     text: String,
     onChecked: (Boolean) -> Unit,
-    selectedColor: Color = Color.DarkGray
 ) {
-    val chipColor = if (isSelected) selectedColor else Color.Transparent
-    val chipTextColor = if (isSelected) Color.White else Color.Unspecified
+    val chipColor: Color = if (isSelected) Orange else Color.Transparent
+    val chipTextColor = if (isSelected) Color.White else Color.White
 
-    Row(
+    Card(
         modifier = Modifier
             .padding(4.dp)
-            .clip(ShapeDefaults.Medium)
-            .background(color = chipColor)
+            .clip(CircleShape)
             .clickable { onChecked(!isSelected) }
             .border(
                 width = 1.dp,
-                color = if (isSelected) selectedColor else Color.LightGray,
-                shape = ShapeDefaults.Medium
-            )
-            .padding(horizontal = 12.dp, vertical = 8.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center
+                color = if (isSelected) Orange else Color.White,
+                shape = CircleShape
+            ),
+        colors = CardDefaults.cardColors(
+            contentColor = Color.White,
+            containerColor = chipColor
+        )
     ) {
         Text(
+            modifier = Modifier.padding(horizontal = 12.dp, vertical = 8.dp),
             text = text,
             color = chipTextColor
         )
--- app/src/main/java/com/example/cinematickets/composable/Icons.kt
+++ app/src/main/java/com/example/cinematickets/composable/Icons.kt
@@ -11,17 +11,23 @@ package com.example.cinematickets.composable
 
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 
 @Composable
 fun CustomIcon(
-    imageVector: ImageVector,
+    modifier: Modifier = Modifier,
+    drawableResId: Int,
     contentDescription: String? = null,
     tint: Color = Color.Unspecified
 ) {
+    val painter = painterResource(drawableResId)
     Icon(
-        imageVector = imageVector,
+        modifier = modifier,
+        painter = painter,
         contentDescription = contentDescription,
         tint = tint
     )
