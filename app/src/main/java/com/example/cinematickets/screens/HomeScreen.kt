8920c62a567309f498d101c348d395dd8a10bf2e
Mohamed Naser <mahmmedn19@gmail.com>
2023-07-06 23:40:01 +0300

    refactor home screen

--- app/src/main/java/com/example/cinematickets/screens/HomeScreen.kt
+++ app/src/main/java/com/example/cinematickets/screens/HomeScreen.kt
@@ -14,7 +14,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -25,6 +27,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -33,11 +36,13 @@ import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.util.lerp
 import com.example.cinematickets.R
 import com.example.cinematickets.Utils
 import com.example.cinematickets.composable.CustomIcon
@@ -47,6 +52,7 @@ import com.example.cinematickets.composable.ResizableImage
 import com.example.cinematickets.composable.SuggestTag
 import com.example.cinematickets.composable.TextChip
 import com.example.cinematickets.composable.VerticalSpacer
+import kotlin.math.absoluteValue
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -59,19 +65,71 @@ fun HomeScreen() {
     val randomImageUrls = Utils.generateRandomImageUrls(10)
     val selectedChipIndex = remember { mutableStateOf(0) }
     val pagerState = rememberPagerState(images.size)
-    val currentIndex = remember { mutableStateOf(1) }
+    BackgroundImageBlur(images, pagerState.currentPage)
 
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        VerticalSpacer(height = 32.dp)
+        ChipSelected(selectedChipIndex)
+
+        HorizontalPager(
+            pageCount = images.size,
+            state = pagerState,
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp),
+            pageSpacing = 16.dp
+        ) { page ->
+            Box(
+                modifier = Modifier
+                    .aspectRatio(4.4f / 5.5f)
+                    .graphicsLayer {
+                        val pageOffset =
+                            ((pagerState.currentPage - page) + pagerState.currentPageOffsetFraction).absoluteValue
+
+                        alpha = lerp(
+                            start = 0.7f,
+                            stop = 1f,
+                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                        )
+
+                        scaleY = lerp(
+                            start = 0.8f,
+                            stop = 1f,
+                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                        )
+                    },
+            ) {
+                MovieImage(images[page])
+            }
+        }
+
+        MovieDetails()
+
+        SuggestTag()
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun PreviewScreen() {
+    HomeScreen()
+}
+
+@Composable
+private fun BackgroundImageBlur(images: List<Int>, currentPage: Int) {
     Box(modifier = Modifier.fillMaxWidth()) {
-        if (pagerState.currentPage in images.indices) {
+        if (currentPage in images.indices) {
             ResizableImage(
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(400.dp)
                     .blur(radius = 20.dp),
-                painter = painterResource(id = images[pagerState.currentPage])
+                painter = painterResource(id = images[currentPage])
             )
         }
-        // Gradient mask
         Box(
             modifier = Modifier
                 .fillMaxSize()
@@ -86,82 +144,64 @@ fun HomeScreen() {
                     )
                 )
         )
-
-    }
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        VerticalSpacer(height = 32.dp)
-        Row {
-            TextChip(
-                isSelected = selectedChipIndex.value == 0,
-                text = "Now Showing",
-                onChecked = { selectedChipIndex.value = 0 }
-            )
-            TextChip(
-                isSelected = selectedChipIndex.value == 1,
-                text = "Coming Soon",
-                onChecked = { selectedChipIndex.value = 1 }
-            )
-        }
-        HorizontalPager(
-            pageCount = images.size,
-            state = pagerState,
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically
-        ) { page ->
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(400.dp)
-                    .padding(32.dp)
-            ) {
-                val imageModifier = Modifier
-                    .fillMaxSize()
-                    .clip(RoundedCornerShape(16.dp)) // Round the corners of the image
-
-                Image(
-                    painter = painterResource(id = images[page]),
-                    contentDescription = null,
-                    contentScale = ContentScale.Crop,
-                    modifier = imageModifier
-                )
-            }
-        }
-
-        Row(
-            modifier = Modifier.padding(
-                vertical = 4.dp, horizontal = 8.dp
-            ),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            CustomIcon(
-                drawableResId = R.drawable.ic_clock_circle,
-                tint = Color.Gray
-            )
-            HorizontalSpacer(width = 4.dp)
-            Text(
-                text = "2h 23m",
-            )
-        }
-        VerticalSpacer(height = 16.dp)
-        CustomText(
-            text = "Fantastic Fantastic : the  ghgf dfhfd Fantastic Fantastic ",
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 64.dp),
-            fontSize = 20.sp,
-        )
-        VerticalSpacer(height = 16.dp)
-        SuggestTag()
     }
 }
 
-@Preview(showSystemUi = true, showBackground = true)
 @Composable
-fun PreviewScreen() {
-    HomeScreen()
+private fun ChipSelected(selectedChipIndex: MutableState<Int>) {
+    Row {
+        TextChip(
+            isSelected = selectedChipIndex.value == 0,
+            text = "Now Showing",
+            onChecked = { selectedChipIndex.value = 0 }
+        )
+        TextChip(
+            isSelected = selectedChipIndex.value == 1,
+            text = "Coming Soon",
+            onChecked = { selectedChipIndex.value = 1 }
+        )
+    }
+}
+
+@Composable
+private fun MovieImage(imageResId: Int) {
+    val imageModifier = Modifier
+        .fillMaxSize()
+        .clip(RoundedCornerShape(16.dp))
+
+    Image(
+        painter = painterResource(id = imageResId),
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        modifier = imageModifier
+    )
+}
+
+@Composable
+private fun MovieDetails() {
+    Row(
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        CustomIcon(
+            drawableResId = R.drawable.ic_clock_circle,
+            tint = Color.Gray
+        )
+        HorizontalSpacer(width = 4.dp)
+        Text(
+            text = "2h 23m",
+        )
+    }
+
+    VerticalSpacer(height = 16.dp)
+
+    CustomText(
+        text = "Fantastic Fantastic : the ghgf dfhfd Fantastic Fantastic",
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 64.dp),
+        fontSize = 20.sp,
+    )
+
+    VerticalSpacer(height = 16.dp)
 }
