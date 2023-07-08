db6876423d1f0d7762a90a7f85cbf423c5820f70
Mohamed Naser <mahmmedn19@gmail.com>
2023-07-08 12:02:59 +0300

    refactor TicketsScreent

--- app/src/main/java/com/example/cinematickets/screens/TicketsScreen.kt
+++ app/src/main/java/com/example/cinematickets/screens/TicketsScreen.kt
@@ -20,13 +20,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
